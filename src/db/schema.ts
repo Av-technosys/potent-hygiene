c0985355b227759d1a392468f3303bbd1f0540dc
Ashish Bishnoi <bishnoi11011@gmail.com>
2026-05-20 12:26:18 +0530

    ashish/featured-migration

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,3 @@
-
-
 import {
   boolean,
   pgTable,
@@ -14,7 +12,20 @@ import {
   jsonb,
   pgEnum
 } from "drizzle-orm/pg-core";
-import { cancelRequestStatusEnum, returnRequestStatusEnum } from "./enum";
+
+export const cancelRequestStatusEnum = pgEnum("cancel_request_status", [
+  "pending",
+  "approved",
+  "rejected",
+  "refunded",
+]);
+
+export const returnRequestStatusEnum = pgEnum("return_request_status", [
+  "pending",
+  "approved",
+  "rejected",
+  "refunded",
+]);
 
 
 // ================= USERS =================
